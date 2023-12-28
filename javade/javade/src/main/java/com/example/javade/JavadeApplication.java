604742df42ea913a1f3fa07de643bbf4e347ffbc
charanandra <64776515+charanandra@users.noreply.github.com>
2023-12-27 18:38:25 -0600

    Add files via upload

---
+++ javade/javade/src/main/java/com/example/javade/JavadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadeApplication.class, args);
+	}
+
+}
---
+++ javade/javade/src/test/java/com/example/javade/JavadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
